06f56738686be22a71453671a79fb14f13d28a9c
Marc Reisner <reisner.marc@gmail.com>
2020-05-31 23:17:57 -0500

    Add some documentation and doctests for public methods

--- rustendo_lib/src/rp2a03.rs
+++ rustendo_lib/src/rp2a03.rs
@@ -10,6 +10,17 @@ pub struct Rp2a03 {
 }
 
 impl Rp2a03 {
+    /// Create a new RP2A03 microprocessor.
+    ///
+    /// Optionally, initialize ram with a slice.
+    ///
+    /// ```
+    /// use rustendo_lib::rp2a03::Rp2a03;
+    /// // Initialize without memory
+    /// let mut rp2a03 = Rp2a03::new(None);
+    /// // Initialize with memory
+    /// let mut rp2a03 = Rp2a03::new(Some(&[0x0, 0x1, 0x2]));
+    /// ```
     pub fn new(mem: Option<&[u8]>) -> Self {
         let mut ram = Box::new(Ram::new());
         let mut bus = Bus::new();
@@ -29,6 +40,20 @@ impl Rp2a03 {
         }
     }
 
+    /// Run the microprocessor for one clock cycle.
+    /// Returns whether the current instruction is complete.
+    ///
+    /// Really, everything happens in the first clock
+    /// cycle and the remaining time is spent doing
+    /// nothing. The clock cycles are needed to emulate
+    /// the microprocessor correctly.
+    ///
+    /// ```
+    /// use rustendo_lib::rp2a03::Rp2a03;
+    /// let mut rp2a03 = Rp2a03::new(None);
+    /// // Run until one instruction completes.
+    /// while rp2a03.clock() {}
+    /// ```
     pub fn clock(&mut self) -> bool {
         self.mos6502.clock()
     }
