10a8390108236acf9e243155153c84a5d74a2b99
Marc Reisner <reisner.marc@gmail.com>
2020-06-13 19:14:54 -0500

    Add some animated static to the page to be filled in with an actual animation later

--- rustendo/src/lib.rs
+++ rustendo/src/lib.rs
@@ -1,5 +1,7 @@
 use rustendo_lib::nes::Nes;
+use std::cell::RefCell;
 use std::f64;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -10,6 +12,16 @@ mod utils;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut(f64)>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
 
 #[wasm_bindgen]
 pub fn render() {
@@ -27,28 +39,70 @@ pub fn render() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.begin_path();
+    let f = Rc::new(RefCell::new(None));
+    let g = Rc::clone(&f);
 
-    // Draw the outer circle.
+    let mut prev_timestamp = 0.0;
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move |timestamp| {
+        web_sys::console::log(&js_sys::Array::of1(&JsValue::from_f64(
+            timestamp - prev_timestamp,
+        )));
+        draw(&context, &canvas);
+        request_animation_frame(f.borrow().as_ref().unwrap());
+        prev_timestamp = timestamp;
+    }) as Box<dyn FnMut(f64)>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
+}
+
+fn draw(context: &web_sys::CanvasRenderingContext2d, canvas: &web_sys::HtmlCanvasElement) {
+    let image_data = context
+        .get_image_data(0.0, 0.0, canvas.width().into(), canvas.height().into())
+        .expect("failed to get ImageData");
+
+    let mut data = image_data.data();
+
+    for y in 0..image_data.height() {
+        for x in 0..image_data.width() {
+            let color: (u8, u8, u8, u8) = if js_sys::Math::random() < 0.5 {
+                (0, 0, 0, 255)
+            } else {
+                (255, 255, 255, 255)
+            };
+
+            let indices = get_color_indices_for_coord(x, y, canvas.width());
+            data[indices.0] = color.0;
+            data[indices.1] = color.1;
+            data[indices.2] = color.2;
+            data[indices.3] = color.3;
+        }
+    }
+
+    let image_data = web_sys::ImageData::new_with_u8_clamped_array(
+        wasm_bindgen::Clamped(&mut data),
+        canvas.width(),
+    )
+    .expect("could not create image data");
     context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
+        .put_image_data(&image_data, 0.0, 0.0)
+        .expect("could not put image data");
+}
 
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+fn get_color_indices_for_coord(x: u32, y: u32, width: u32) -> (usize, usize, usize, usize) {
+    let x = x as usize;
+    let y = y as usize;
+    let width = width as usize;
+    let red_index = y * (width * 4) + x * 4;
+    (red_index, red_index + 1, red_index + 2, red_index + 3)
+}
 
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
+trait DrawPixel {
+    fn draw_pixel(&self, x: f64, y: f64, color: &str);
+}
 
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
+impl DrawPixel for web_sys::CanvasRenderingContext2d {
+    fn draw_pixel(&self, x: f64, y: f64, color: &str) {
+        self.set_fill_style(&JsValue::from_str(color));
+        self.fill_rect(x, y, 1.0, 1.0);
+    }
 }
