dd1d3a409cf550a0a65e66f0756826f13a7c0ddc
Marc Reisner <reisner.marc@gmail.com>
2020-06-13 22:27:15 -0500

    Refactor a bit

--- rustendo/src/lib.rs
+++ rustendo/src/lib.rs
@@ -1,9 +1,11 @@
+use js_sys::{Array, Math};
 use rustendo_lib::nes::Nes;
 use std::cell::RefCell;
 use std::f64;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{Clamped, JsCast};
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement, ImageData, Window};
 
 mod utils;
 
@@ -13,7 +15,7 @@ mod utils;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-fn window() -> web_sys::Window {
+fn window() -> Window {
     web_sys::window().expect("no global `window` exists")
 }
 
@@ -27,8 +29,8 @@ fn request_animation_frame(f: &Closure<dyn FnMut(f64)>) {
 pub fn render() {
     let document = web_sys::window().unwrap().document().unwrap();
     let canvas = document.get_element_by_id("rustendo-canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
+    let canvas: HtmlCanvasElement = canvas
+        .dyn_into::<HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
 
@@ -36,7 +38,7 @@ pub fn render() {
         .get_context("2d")
         .unwrap()
         .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .dyn_into::<CanvasRenderingContext2d>()
         .unwrap();
 
     let f = Rc::new(RefCell::new(None));
@@ -44,9 +46,7 @@ pub fn render() {
 
     let mut prev_timestamp = 0.0;
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move |timestamp| {
-        web_sys::console::log(&js_sys::Array::of1(&JsValue::from_f64(
-            timestamp - prev_timestamp,
-        )));
+        web_sys::console::log(&Array::of1(&JsValue::from_f64(timestamp - prev_timestamp)));
         draw(&context, &canvas);
         request_animation_frame(f.borrow().as_ref().unwrap());
         prev_timestamp = timestamp;
@@ -55,7 +55,7 @@ pub fn render() {
     request_animation_frame(g.borrow().as_ref().unwrap());
 }
 
-fn draw(context: &web_sys::CanvasRenderingContext2d, canvas: &web_sys::HtmlCanvasElement) {
+fn draw(context: &CanvasRenderingContext2d, canvas: &HtmlCanvasElement) {
     let image_data = context
         .get_image_data(0.0, 0.0, canvas.width().into(), canvas.height().into())
         .expect("failed to get ImageData");
@@ -64,36 +64,38 @@ fn draw(context: &web_sys::CanvasRenderingContext2d, canvas: &web_sys::HtmlCanva
 
     for y in 0..image_data.height() {
         for x in 0..image_data.width() {
-            let color: (u8, u8, u8, u8) = if js_sys::Math::random() < 0.5 {
-                (0, 0, 0, 255)
+            let color = if Math::random() < 0.5 {
+                (0, 0, 0)
             } else {
-                (255, 255, 255, 255)
+                (255, 255, 255)
             };
 
-            let indices = get_color_indices_for_coord(x, y, canvas.width());
-            data[indices.0] = color.0;
-            data[indices.1] = color.1;
-            data[indices.2] = color.2;
-            data[indices.3] = color.3;
+            set_color_at_coord(&mut data, canvas.width(), x, y, color)
         }
     }
 
-    let image_data = web_sys::ImageData::new_with_u8_clamped_array(
-        wasm_bindgen::Clamped(&mut data),
-        canvas.width(),
-    )
-    .expect("could not create image data");
+    let image_data = ImageData::new_with_u8_clamped_array(Clamped(&mut data), canvas.width())
+        .expect("could not create image data");
     context
         .put_image_data(&image_data, 0.0, 0.0)
         .expect("could not put image data");
 }
 
-fn get_color_indices_for_coord(x: u32, y: u32, width: u32) -> (usize, usize, usize, usize) {
+fn set_color_at_coord(
+    data: &mut Clamped<Vec<u8>>,
+    width: u32,
+    x: u32,
+    y: u32,
+    color: (u8, u8, u8),
+) {
     let x = x as usize;
     let y = y as usize;
     let width = width as usize;
     let red_index = y * (width * 4) + x * 4;
-    (red_index, red_index + 1, red_index + 2, red_index + 3)
+    data[red_index] = color.0;
+    data[red_index + 1] = color.1;
+    data[red_index + 2] = color.2;
+    data[red_index + 3] = 0xFF;
 }
 
 trait DrawPixel {
