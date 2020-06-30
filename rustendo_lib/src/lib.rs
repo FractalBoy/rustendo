811b3ed1ee295e76503b016b919e027f5de410c5
Marc Reisner <reisner.marc@gmail.com>
2020-06-30 00:10:55 -0500

    Add conditional logging based on DEBUG flag

--- rustendo_lib/src/lib.rs
+++ rustendo_lib/src/lib.rs
@@ -1,22 +1,27 @@
 #[macro_export]
 macro_rules! log {
     ( $( $t:tt )* ) => {
-        if cfg!(target_arch = "wasm32") { 
-            web_sys::console::log_1(&format!( $( $t )* ).into());
-        } else {
-            print!( $( $t )* );
+        let debug = option_env!("DEBUG");
+        if let Some(debug) = debug {
+            if debug == "1" {
+                if cfg!(target_arch = "wasm32") {
+                    web_sys::console::log_1(&format!( $( $t )* ).into());
+                } else {
+                    print!( $( $t )* );
+                }
+            }
         }
     }
 }
 
 mod assembler;
-mod cpu_bus;
-mod ppu_bus;
-mod mos6502;
-mod cpu_ram;
-mod ppu_ram;
 pub mod cartridge;
-mod ricoh2c02;
-mod mappers;
-pub mod nes;
 mod controller;
+mod cpu_bus;
+mod cpu_ram;
+mod mappers;
+mod mos6502;
+pub mod nes;
+mod ppu_bus;
+mod ppu_ram;
+mod ricoh2c02;
